a58224e2daa700aa74d124534c67d1468ea72f77
paek1127 <paek1127@gmail.com>
2025-02-14 14:34:03 +0900

    feat: 5단계

--- src/main/kotlin/me/sh/App.kt
+++ src/main/kotlin/me/sh/App.kt
@@ -4,9 +4,8 @@ class App {
     fun run() {
         println("== 명언 앱 ==")
 
-        var wiseSayings: MutableList<WiseSaying> = mutableListOf()
-
         var lastId = 1
+        var wiseSayings = mutableListOf<WiseSaying>()
 
         while (true) {
             print("명령) ")
@@ -22,7 +21,23 @@ class App {
                 val author = readlnOrNull()!!.trim()
 
                 val id = lastId++
+
+                wiseSayings.add(WiseSaying(id, content, author))
+
                 println("${id}번 명언이 등록되었습니다.")
+            }
+            else if (input == "목록") {
+                if (wiseSayings.isEmpty()) {
+                    println("등록된 명언이 없습니다.")
+                    continue
+                }
+
+                println("번호 / 작가 / 명언")
+                println("----------------------")
+
+                wiseSayings.forEach {
+                    println("${it.id} / ${it.author} / ${it.content}")
+                }
             }
         }
     }
---
+++ src/main/kotlin/me/sh/WiseSaying.kt
@@ -0,0 +1,9 @@
+package me.sh
+
+class WiseSaying(
+    val id: Int,
+    var content: String,
+    var author: String,
+) {
+
+}
