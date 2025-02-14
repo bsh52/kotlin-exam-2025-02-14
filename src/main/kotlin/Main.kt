729512e044bd3a45796600693cf035fe3b2bc273
paek1127 <paek1127@gmail.com>
2025-02-14 12:42:24 +0900

    feat: scanner 대신 readln 을 사용, 개발자가 느낌표 두개로 nullable을 명시적으로 제거

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package me.sh
 
 fun main() {
-    println("Hello World!")
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명언) ")
+
+        val input = readlnOrNull()!!.trim()
+
+        if (input == "종료")
+            break
+    }
 }
