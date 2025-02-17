c4a99724a3412bdbcf02b6b26d8f02a051a10055
bsh <paek1127@gmail.com>
2025-02-17 11:15:41 +0900

    feat: WiseSayingMemoryRepository 추가

--- src/main/kotlin/me/sh/global/bean/SingletonScope.kt
+++ src/main/kotlin/me/sh/global/bean/SingletonScope.kt
@@ -2,12 +2,13 @@ package me.sh.global.bean
 
 import me.sh.domain.system.system.controller.SystemController
 import me.sh.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+import me.sh.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import me.sh.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import me.sh.domain.wiseSaying.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
     val wiseSayingController by lazy { WiseSayingController() }
     val wiseSayingService by lazy { WiseSayingService() }
-    val wiseSayingRepository by lazy { WiseSayingRepository() }
+    val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
     val systemController by lazy { SystemController() }
 }
