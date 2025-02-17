dfc04881ee05def2d1009a4398492954f351e43d
bsh <paek1127@gmail.com>
2025-02-17 11:30:57 +0900

    feat: WiseSayingFileRepository 추가

--- src/main/kotlin/me/sh/global/bean/SingletonScope.kt
+++ src/main/kotlin/me/sh/global/bean/SingletonScope.kt
@@ -2,6 +2,7 @@ package me.sh.global.bean
 
 import me.sh.domain.system.system.controller.SystemController
 import me.sh.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+import me.sh.domain.wiseSaying.wiseSaying.repository.WiseSayingFileRepository
 import me.sh.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import me.sh.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import me.sh.domain.wiseSaying.wiseSaying.service.WiseSayingService
@@ -10,5 +11,6 @@ object SingletonScope {
     val wiseSayingController by lazy { WiseSayingController() }
     val wiseSayingService by lazy { WiseSayingService() }
     val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
+    val wiseSayingFileRepository by lazy { WiseSayingFileRepository() }
     val systemController by lazy { SystemController() }
 }
